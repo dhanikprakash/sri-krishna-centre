c53676e76e4a0a2517daf9e8f4c8ac584522cf7c
dhanikprakash <dhanikprakash@gmail.com>
2022-05-17 20:56:47 +0100

    Update the images

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Cards.css";
 import CardItem from "./CardItem";
-import img9 from "../../images/img-9.jpg";
-import img2 from "../../images/img-2.jpg";
-import img3 from "../../images/img-3.jpg";
-import img4 from "../../images/img-4.jpg";
-import img8 from "../../images/img-8.jpg";
+import img1 from "../../images/IMG_1.jpg";
+import img2 from "../../images/IMG_2.jpg";
+import img3 from "../../images/IMG_3.jpg";
+import img4 from "../../images/IMG_4.jpg";
+import img5 from "../../images/IMG_5.jpg";
 
 function Cards() {
   return (
@@ -15,13 +15,13 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src={img9}
-              text="Nam ullamcorper erat vel ligula porta tristique. Proin vestibulum dui a mattis maximus. Morbi hendrerit dui non auctor auctor. Duis molestie massa in tellus faucibus, a accumsan purus pulvinar. Quisque luctus ante vel risus sollicitudin molestie a nec neque. Donec neque arcu, condimentum id arcu ut, fringilla pharetra ligula."
-              label="Adventure"
+              src={img2}
+              text="Higly skilled surgens, Dr Sarath T S completed MBBS and MS (General Surgery) from Thiruvananthapuram Medical college, MCh (Plastic Surgery) from Kottayam Medical College, Dr Dhanya Prakash completed BDS from RMDCH, Annamalai University, MDS from Azeezia Dental College and PGCE from Annamalai University, Dr sreelal sreedharan completed M.B.B.S and M.S General Surgery from Trivandrum Medical College and MCh plastic surgery from Kottayam Medical College."
+              label="Hair Transplantation"
               path="/services"
             />
             <CardItem
-              src={img2}
+              src={img3}
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend tincidunt massa, vel ultrices dui rhoncus in. Suspendisse dictum leo a quam ultricies sagittis. Nam pharetra tristique justo at luctus. Sed convallis est non elit pellentesque, vitae fermentum augue lacinia. Vivamus gravida sed ante a vehicula."
               label="Luxury"
               path="/services"
@@ -29,21 +29,21 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src={img3}
+              src={img4}
               text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-              label="Mystery"
+              label="Sri Krishna"
               path="/services"
             />
             <CardItem
-              src={img4}
+              src={img5}
               text="Experience Football on Top of the Himilayan Mountains"
-              label="Adventure"
+              label="Cosmetic Surgeries"
               path="/products"
             />
             <CardItem
-              src={img8}
+              src={img1}
               text="Ride through the Sahara Desert on a guided camel tour"
-              label="Adrenaline"
+              label="Sri Krishna"
               path="/products"
             />
           </ul>
